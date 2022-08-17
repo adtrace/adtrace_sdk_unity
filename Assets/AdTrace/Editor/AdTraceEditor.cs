d8080c337734230db1d5295573770f91f29818ef
Nasser Amini <namini40@gmail.com>
2022-08-17 14:21:11 +0430

    Editors imports optimized

--- Assets/AdTrace/Editor/AdTraceEditor.cs
+++ Assets/AdTrace/Editor/AdTraceEditor.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Xml;
-using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEditor;
