3e6c520377f9ec9929b75ed31df7bdf126db8bc3
Nasser Amini <namini40@gmail.com>
2022-08-20 10:40:21 +0430

    add url strategies

---
+++ Assets/AdTrace/Unity/AdTraceUrlStrategy.cs
@@ -0,0 +1,30 @@
+﻿namespace io.adtrace.sdk
+{
+    [System.Serializable]
+    public enum AdTraceUrlStrategy
+    {
+        Default,
+        DataResidencyEU,
+        DataResidencyTK,
+        DataResidencyUS,
+        India,
+        China,
+    }
+
+    public static class AdTraceUrlStrategyExtension
+    {
+        public static string ToLowerCaseString(this AdTraceUrlStrategy strategy)
+        {
+            switch (strategy)
+            {
+                case AdTraceUrlStrategy.India: return "india";
+                case AdTraceUrlStrategy.China: return "china";
+                case AdTraceUrlStrategy.DataResidencyEU: return "data-residency-eu";
+                case AdTraceUrlStrategy.DataResidencyTK: return "data-residency-tr";
+                case AdTraceUrlStrategy.DataResidencyUS: return "data-residency-us";
+                default: return string.Empty;
+            }
+        }
+    }
+}
+
